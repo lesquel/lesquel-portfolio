025f138bf1caf6428bfee8cf44cc4e2304fc72e4
lesquel <lesquel1319@gmail.com>
2026-02-06 16:03:39 -0500

    feat: update Supabase URL in environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'aws-1-us-east-2.pooler.supabase.com',
+  supabaseUrl: 'https://busthllptesobcetuzwb.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ1c3RobGxwdGVzb2JjZXR1endiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA0MDM3NzksImV4cCI6MjA4NTk3OTc3OX0.p07Ov7KbCd8s5K0CwxgXQlp06ZizXFvP_lX6N8GbFfI',
 };
